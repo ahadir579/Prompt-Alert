21a7acb04b8e764d49eb1b4199bf856208a65233
ahadir579 <139805536+ahadir579@users.noreply.github.com>
2023-07-31 06:47:08 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+//use of prompt & alert
+function getUserInput() {
+    let name = prompt("Please enter your name");
+    if (name) {
+        alert(`Congratulation! ${name} thank you for visiting our website.`);
+    }
+    else {
+        alert(`Congratulation! Anonymouse! thank you for visiting our website`);
+    }
+}
---
+++ index.ts
@@ -0,0 +1,14 @@
+//use of prompt & alert
+
+function getUserInput(){
+    let name = prompt("Please enter your name");
+if (name){
+    alert(`Congratulation! ${name} thank you for visiting our website.`)
+}
+else
+{
+    alert(`Congratulation! Anonymouse! thank you for visiting our website`)
+}
+}
+
+
